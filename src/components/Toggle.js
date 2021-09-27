65c8c2bd27a89d965ddfa7044f75f93588e12906
troymeeker <troymeeker1@gmail.com>
2021-09-27 11:04:44 -0700

    complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState }  from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  //set initial value to false, bc the button should be off when page is first loaded
+
+ function handleClick(){
+   setIsOn((isOn)=>!isOn);
+ }
+ const color = isOn ? "green" : "red";
+  return <button style={{background: color}}onClick={handleClick }>{isOn ? "ON" : "OFF"}</button>;
+  //use the state variable in the text of the component using conditional rendering
 }
 
 export default Toggle;
